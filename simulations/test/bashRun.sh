2256ebca6ebc251fbe4ff4faadf61ad86a8aab83
AngeloTrotta85 <angelo@magilia.com>
2016-11-02 18:09:41 +0100

    add bash script

---
+++ simulations/test/bashRun.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#cd /home/angelo/git/inet_ext/simulations/test
+
+#for ((i=1; $i<3; i++)); do
+for i in {1..3840}
+do
+	echo "RUN $i"
+	opp_run -r $i -u Cmdenv -c TestMobMassMobility -n ../../src:..:../../../../Programs/omnetpp-5.0/samples/inet/examples:../../../../Programs/omnetpp-5.0/samples/inet/src:../../../../Programs/omnetpp-5.0/samples/inet/tutorials -l ../../src/inet_ext -l ../../../../Programs/omnetpp-5.0/samples/inet/src/INET --debug-on-errors=false omnetpp.ini
+done
