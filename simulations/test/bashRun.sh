7ed574b8259b69cf52d674399381dec45f95571d
AngeloTrotta85 <angelo@magilia.com>
2016-11-08 14:07:27 +0100

    ok

--- simulations/test/bashRun.sh
+++ simulations/test/bashRun.sh
@@ -3,9 +3,9 @@
 #cd /home/angelo/git/inet_ext/simulations/test
 
 #for ((i=1; $i<3; i++)); do
-for i in {173..1728}
+for i in {0..2160}
 do
 	echo "RUN $i"
-	#opp_run -r $i -u Cmdenv -c TestMobMassMobility -n ../../src:..:../../../../Programs/omnetpp-5.0/samples/inet/examples:../../../../Programs/omnetpp-5.0/samples/inet/src:../../../../Programs/omnetpp-5.0/samples/inet/tutorials -l ../../src/inet_ext -l ../../../../Programs/omnetpp-5.0/samples/inet/src/INET --debug-on-errors=false omnetpp.ini
-	opp_run -r $i -u Cmdenv -c TestMobGaussMarkovMobility -n ../../src:..:../../../../Programs/omnetpp-5.0/samples/inet/examples:../../../../Programs/omnetpp-5.0/samples/inet/src:../../../../Programs/omnetpp-5.0/samples/inet/tutorials -l ../../src/inet_ext -l ../../../../Programs/omnetpp-5.0/samples/inet/src/INET --debug-on-errors=false omnetpp.ini
+	opp_run -r $i -u Cmdenv -c TestMobMassMobility -n ../../src:..:../../../../Programs/omnetpp-5.0/samples/inet/examples:../../../../Programs/omnetpp-5.0/samples/inet/src:../../../../Programs/omnetpp-5.0/samples/inet/tutorials -l ../../src/inet_ext -l ../../../../Programs/omnetpp-5.0/samples/inet/src/INET --debug-on-errors=false omnetpp.ini
+	#opp_run -r $i -u Cmdenv -c TestMobGaussMarkovMobility -n ../../src:..:../../../../Programs/omnetpp-5.0/samples/inet/examples:../../../../Programs/omnetpp-5.0/samples/inet/src:../../../../Programs/omnetpp-5.0/samples/inet/tutorials -l ../../src/inet_ext -l ../../../../Programs/omnetpp-5.0/samples/inet/src/INET --debug-on-errors=false omnetpp.ini
 done
